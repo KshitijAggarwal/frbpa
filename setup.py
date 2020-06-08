9136544437f8a2f49a26d4e39894120e1133f14e
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-06-08 11:37:54 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='frbpa',
+    version='0.1',
+    packages=['frbpa'],
+    url='https://github.com/KshitijAggarwal/frbpa',
+    author='Kshitij Aggarwal',
+    author_email='ka0064@mix.wvu.edu',
+    license='',
+    description='Periodicity Analysis of Repeating FRBs',
+    install_requires=['astropy', 'numpy', 'matplotlib', 'tqdm', 'scipy', 'P4J'],
+)
